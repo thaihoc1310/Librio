42bf2ce5a21a2a3c8af9886be224580acfc0874f
thaihoc <thaihoc131005@gmail.com>
2024-10-04 15:00:05 +0700

    merge test

--- src/main/java/librio/HelloApplication.java
+++ src/main/java/librio/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Librio");
         stage.setScene(scene);
         stage.show();
