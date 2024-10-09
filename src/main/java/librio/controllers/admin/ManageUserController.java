1e1434adc82225efa93d5b19fffc89c8318bf5e8
GiangDurian <gianglessin2@gmail.com>
2024-10-09 23:25:15 +0700

    update

--- src/main/java/librio/controllers/admin/ManageUserController.java
+++ src/main/java/librio/controllers/admin/ManageUserController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Callback;
@@ -89,6 +90,13 @@ public class ManageUserController implements Initializable {
                             User user = getTableView().getItems().get(getIndex());
                             openDetailUserScene(user);
                         });
+
+                        btnUpdate.setOnAction(event -> {
+                            User user = getTableView().getItems().get(getIndex());
+                            // Fetch updated user data from the database
+                            User updatedUser = getUserById(user.getId());
+                            openUpdateUserScene(updatedUser);
+                        });
                     }
 
                     @Override
@@ -107,6 +115,119 @@ public class ManageUserController implements Initializable {
             }
         };
         actionColumn.setCellFactory(cellFactory);
+    }
+
+    private User getUserById(String userId) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = "SELECT * FROM users WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String phoneNumber = resultSet.getString("phone_number");
+                String address = resultSet.getString("address") ;
+                Gender gender = Gender.valueOf(resultSet.getString("gender").toUpperCase());
+                Role role = Role.valueOf(resultSet.getString("role").toUpperCase());
+
+                return new User(id, name, email, phoneNumber, address, gender, role);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void loadUsersFromDatabase() {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            userList = FXCollections.observableArrayList();
+            String query = "SELECT * FROM users";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String phoneNumber = resultSet.getString("phone_number");
+                Gender gender = Gender.valueOf(resultSet.getString("gender").toUpperCase());
+                Role role = Role.valueOf(resultSet.getString("role").toUpperCase());
+
+                User user = new User(id, name, email, phoneNumber, gender, role);
+                userList.add(user);
+            }
+
+            userTableView.setItems(userList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void filterUsers(String keyword) {
+        if (keyword == null || keyword.isEmpty()) {
+            userTableView.setItems(userList);
+        } else {
+            ObservableList<User> filteredList = FXCollections.observableArrayList();
+            for (User user : userList) {
+                if (user.getName().toLowerCase().contains(keyword.toLowerCase()) ||
+                        user.getEmail().toLowerCase().contains(keyword.toLowerCase())) {
+                    filteredList.add(user);
+                }
+            }
+            userTableView.setItems(filteredList);
+        }
+    }
+
+    @FXML
+    private void openCreateUserScene() {
+        try {
+            // Tải FXML của scene mới
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin/CreateUser.fxml"));
+            Parent root = loader.load();
+
+            // Tạo stage mới cho scene
+            Stage stage = new Stage();
+            stage.setTitle("Create New User");
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.initOwner(userTableView.getScene().getWindow());
+            stage.initModality(Modality.WINDOW_MODAL);
+
+            // Hiển thị scene
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openUpdateUserScene(User user) {
+        try {
+            // Tải FXML của scene mới
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin/UpdateUser.fxml"));
+            Parent root = loader.load();
+
+            // Tạo controller và truyền ManageUserController và User vào
+            UpdateUserController updateUserController = loader.getController();
+            updateUserController.setManageUserController(this);
+            updateUserController.setUser(user);
+
+            // Tạo stage mới cho scene
+            Stage stage = new Stage();
+            stage.setTitle("Update User");
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.initOwner(userTableView.getScene().getWindow());
+            stage.initModality(Modality.WINDOW_MODAL);
+            // Hiển thị scene
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Mở scene xóa User:
@@ -147,64 +268,4 @@ public class ManageUserController implements Initializable {
         }
     }
 
-    @FXML
-    private void openCreateUserScene() {
-        try {
-            // Tải FXML của scene mới
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin/CreateUser.fxml"));
-            Parent root = loader.load();
-
-            // Tạo stage mới cho scene
-            Stage stage = new Stage();
-            stage.setTitle("Create New User");
-            stage.setScene(new Scene(root));
-            stage.setResizable(false);
-            stage.initStyle(StageStyle.UTILITY);
-
-            // Hiển thị scene
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadUsersFromDatabase() {
-        try (Connection connection = DatabaseConnection.getConnection()) {
-            userList = FXCollections.observableArrayList();
-            String query = "SELECT * FROM users";
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                String id = resultSet.getString("id");
-                String name = resultSet.getString("name");
-                String email = resultSet.getString("email");
-                String phoneNumber = resultSet.getString("phone_number");
-                Gender gender = Gender.valueOf(resultSet.getString("gender").toUpperCase());
-                Role role = Role.valueOf(resultSet.getString("role").toUpperCase());
-
-                User user = new User(id, name, email, phoneNumber, gender, role);
-                userList.add(user);
-            }
-
-            userTableView.setItems(userList);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void filterUsers(String keyword) {
-        if (keyword == null || keyword.isEmpty()) {
-            userTableView.setItems(userList);
-        } else {
-            ObservableList<User> filteredList = FXCollections.observableArrayList();
-            for (User user : userList) {
-                if (user.getName().toLowerCase().contains(keyword.toLowerCase()) ||
-                        user.getEmail().toLowerCase().contains(keyword.toLowerCase())) {
-                    filteredList.add(user);
-                }
-            }
-            userTableView.setItems(filteredList);
-        }
-    }
 }
