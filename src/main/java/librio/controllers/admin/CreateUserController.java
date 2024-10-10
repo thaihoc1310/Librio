72fd65ef024713cbaf8ad84e9d076d9fa6fe8159
thaihoc <thaihoc131005@gmail.com>
2024-10-10 17:26:43 +0700

    update change avatar in updateController

--- src/main/java/librio/controllers/admin/CreateUserController.java
+++ src/main/java/librio/controllers/admin/CreateUserController.java
@@ -59,30 +59,6 @@ public class CreateUserController implements Initializable {
     private String avatarFilePath;
     private String previousAvatarFilePath;
 
-    public static void cropAndClipToCircle(Image avatarImage, ImageView avatarImageView, double radius) {
-        // Lấy chiều rộng và chiều cao của ảnh
-        double width = avatarImage.getWidth();
-        double height = avatarImage.getHeight();
-
-        // Tính toán kích thước để cắt ảnh thành hình vuông
-        double cropSize = Math.min(width, height);  // Chọn kích thước nhỏ hơn giữa width và height
-
-        // Tính toán tọa độ bắt đầu để cắt hình vuông từ trung tâm của ảnh
-        double x = (width - cropSize) / 2;
-        double y = (height - cropSize) / 2;
-
-        // Cắt ảnh thành hình vuông
-        PixelReader reader = avatarImage.getPixelReader();
-        WritableImage squareImage = new WritableImage(reader, (int) x, (int) y, (int) cropSize, (int) cropSize);
-
-        // Hiển thị ảnh đã cắt trong ImageView
-        avatarImageView.setImage(squareImage);
-        avatarImageView.setPreserveRatio(true);
-
-        // Tạo clip hình tròn với bán kính được cung cấp và tâm tại (radius, radius)
-        Circle clip = new Circle(radius, radius, radius);
-        avatarImageView.setClip(clip);  // Thiết lập clip hình tròn cho ImageView
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -186,6 +162,30 @@ public class CreateUserController implements Initializable {
 
     }
 
+    public static void cropAndClipToCircle(Image avatarImage, ImageView avatarImageView, double radius) {
+        // Lấy chiều rộng và chiều cao của ảnh
+        double width = avatarImage.getWidth();
+        double height = avatarImage.getHeight();
+
+        // Tính toán kích thước để cắt ảnh thành hình vuông
+        double cropSize = Math.min(width, height);  // Chọn kích thước nhỏ hơn giữa width và height
+
+        // Tính toán tọa độ bắt đầu để cắt hình vuông từ trung tâm của ảnh
+        double x = (width - cropSize) / 2;
+        double y = (height - cropSize) / 2;
+
+        // Cắt ảnh thành hình vuông
+        PixelReader reader = avatarImage.getPixelReader();
+        WritableImage squareImage = new WritableImage(reader, (int) x, (int) y, (int) cropSize, (int) cropSize);
+
+        // Hiển thị ảnh đã cắt trong ImageView
+        avatarImageView.setImage(squareImage);
+        avatarImageView.setPreserveRatio(true);
+
+        // Tạo clip hình tròn với bán kính được cung cấp và tâm tại (radius, radius)
+        Circle clip = new Circle(radius, radius, radius);
+        avatarImageView.setClip(clip);  // Thiết lập clip hình tròn cho ImageView
+    }
 
 }
 
