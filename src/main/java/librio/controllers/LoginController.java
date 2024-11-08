ce4ebe082b8ea8b665b50781304e3b5a2c81fb2f
GiangDurian <gianglessin2@gmail.com>
2024-11-09 01:28:01 +0700

    Update registation

--- src/main/java/librio/controllers/LoginController.java
+++ src/main/java/librio/controllers/LoginController.java
@@ -25,6 +25,7 @@ import java.sql.*;
 import java.time.LocalDate;
 
 import static librio.util.DatabaseUtil.authenticate;
+import static librio.util.DatabaseUtil.isEmailExists;
 
 public class LoginController {
 
@@ -55,7 +56,11 @@ public class LoginController {
     @FXML
     private PasswordField signUpPasswordField;
     @FXML
+    private TextField signUpPasswordTextVisible;
+    @FXML
     private PasswordField signUpConfirmPasswordField;
+    @FXML
+    private TextField signUpConfirmPasswordTextVisible;
     @FXML
     private ComboBox<Gender> genderComboBox;
     @FXML
@@ -64,6 +69,18 @@ public class LoginController {
     private ImageView openEyeImage;
     @FXML
     private ImageView closeEyeImage;
+    @FXML
+    private Label nameErrorLabel;
+    @FXML
+    private Label emailErrorLabel;
+    @FXML
+    private Label phoneNumberErrorLabel;
+    @FXML
+    private Label passwordErrorLabel;
+    @FXML
+    private Label confirmPasswordErrorLabel;
+    @FXML
+    private Label genderAndbirthDateErrorLabel;
     @FXML
     private AnchorPane leftPane;
     @FXML
@@ -78,12 +95,31 @@ public class LoginController {
     @FXML
     private void initialize() {
         genderComboBox.setItems(FXCollections.observableArrayList(Gender.values()));
+
         passwordTextVisible.setVisible(false);
+        signUpPasswordTextVisible.setVisible(false);
+        signUpConfirmPasswordTextVisible.setVisible(false);
+
+        openEyeImage1.toFront();
+        closeEyeImage1.toFront();
+        openEyeImage2.toFront();
+        closeEyeImage2.toFront();
+
+        openEyeImage.setOnMouseClicked(event -> showPassword(passwordField, passwordTextVisible, openEyeImage, closeEyeImage));
+        closeEyeImage.setOnMouseClicked(event -> hidePassword(passwordField, passwordTextVisible, openEyeImage, closeEyeImage));
+
+        openEyeImage1.setOnMouseClicked(event -> showPassword(signUpPasswordField, signUpPasswordTextVisible, openEyeImage1, closeEyeImage1));
+        closeEyeImage1.setOnMouseClicked(event -> hidePassword(signUpPasswordField, signUpPasswordTextVisible, openEyeImage1, closeEyeImage1));
+
+        openEyeImage2.setOnMouseClicked(event -> showPassword(signUpConfirmPasswordField, signUpConfirmPasswordTextVisible, openEyeImage2, closeEyeImage2));
+        closeEyeImage2.setOnMouseClicked(event -> hidePassword(signUpConfirmPasswordField, signUpConfirmPasswordTextVisible, openEyeImage2, closeEyeImage2));
+
+
         usernameField.setOnMouseClicked(event -> clearErrorMessage());
         passwordField.setOnMouseClicked(event -> clearErrorMessage());
         switchSignUp.setOnMouseClicked(event -> switchToSignUp());
         switchSignIn.setOnMouseClicked(event -> switchToSignIn());
-        //addListeners();
+        addHideErrorListenersToSignUpFields();
     }
 
     @FXML
@@ -112,6 +148,7 @@ public class LoginController {
 
         transition.play();
         incorrectLoginInformation.setText("");
+        clearFieldData();
     }
 
     @FXML
@@ -140,29 +177,29 @@ public class LoginController {
         });
 
         transition.play();
-
+        hideSignUpErrorLabels();
+        clearFieldData();
     }
 
-    @FXML
-    private void showPassword() {
-        passwordTextVisible.setText(passwordField.getText());
-        passwordTextVisible.setVisible(true);
-        passwordField.setVisible(false);
-        openEyeImage.setVisible(false);
-        closeEyeImage.setVisible(true);
-        passwordTextVisible.requestFocus();
-        passwordTextVisible.positionCaret(passwordField.getText().length());
+
+    private void showPassword(PasswordField passField, TextField passTextVisible, ImageView openEye, ImageView closeEye) {
+        passTextVisible.setText(passField.getText());
+        passTextVisible.setVisible(true);
+        passField.setVisible(false);
+        openEye.setVisible(false);
+        closeEye.setVisible(true);
+        passTextVisible.requestFocus();
+        passTextVisible.positionCaret(passTextVisible.getText().length());
     }
 
-    @FXML
-    private void hidePassword() {
-        passwordField.setText(passwordTextVisible.getText());
-        passwordField.setVisible(true);
-        passwordTextVisible.setVisible(false);
-        openEyeImage.setVisible(true);
-        closeEyeImage.setVisible(false);
-        passwordField.requestFocus();
-        passwordField.positionCaret(passwordField.getText().length());
+    private void hidePassword(PasswordField passField, TextField passTextVisible, ImageView openEye, ImageView closeEye) {
+        passField.setText(passTextVisible.getText());
+        passField.setVisible(true);
+        passTextVisible.setVisible(false);
+        openEye.setVisible(true);
+        closeEye.setVisible(false);
+        passField.requestFocus();
+        passField.positionCaret(passField.getText().length());
     }
 
     private void clearErrorMessage() {
@@ -193,6 +230,56 @@ public class LoginController {
         Gender gender = genderComboBox.getValue();
         LocalDate birthDate = birthDatePicker.getValue();
 
+        boolean validation = false;
+
+        if(name.trim().isEmpty()){
+            nameErrorLabel.setText("Name cannot be empty!");
+            validation = true;
+        }
+
+        if(email.trim().isEmpty()){
+            emailErrorLabel.setText("Email cannot be empty!");
+            validation = true;
+        }else if(isEmailExists(email.trim())){
+            emailErrorLabel.setText("This email already exists!");
+            validation = true;
+        }else if(!email.trim().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")){
+            emailErrorLabel.setText("Invalid email address!");
+            validation = true;
+        }
+
+        if (phoneNumber.trim().isEmpty()) {
+            phoneNumberErrorLabel.setText("Phone number cannot be empty!");
+            validation = true;
+        } else if (!phoneNumber.trim().matches("\\d{10}")) {
+            phoneNumberErrorLabel.setText("Phone number must be 10 digits!");
+            validation = true;
+        }
+
+        if(gender == null || birthDate == null){
+            genderAndbirthDateErrorLabel.setText("Please select your Gender and Birth Date!");
+            validation = true;
+        }else if(birthDate.isAfter(LocalDate.now())){
+            genderAndbirthDateErrorLabel.setText("Birth date must be before now!");
+        }
+
+        if(signUpPassword.isEmpty()){
+            passwordErrorLabel.setText("Password cannot be empty!");
+            validation = true;
+        }
+
+        if(signUpConfirmPassword.isEmpty()){
+            confirmPasswordErrorLabel.setText("Confirm password cannot be empty!");
+            validation = true;
+        }else if(!signUpConfirmPassword.equals(signUpPassword)){
+            confirmPasswordErrorLabel.setText("Passwords do not match!");
+            validation = true;
+        }
+
+        if(validation){
+            return;
+        }
+
         String query = "INSERT INTO users (name, email, password, phone_number, gender, birth_of_date, created_by, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
 
         try (Connection connection = DatabaseConnection.getConnection();
@@ -218,6 +305,7 @@ public class LoginController {
                     memberStatement.setLong(3, 0); // Total books borrowed bắt đầu từ 0
                     memberStatement.executeUpdate();
                 }
+                switchToSignIn();
             }
 
         }catch (SQLException e) {
@@ -243,15 +331,38 @@ public class LoginController {
         currentStage.close();
     }
 
-    private void addListeners() {
+    private void addHideErrorListenersToSignUpFields() {
+        userNameTextField.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        emailTextField.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        phoneNumberTextField.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        signUpPasswordField.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        signUpConfirmPasswordField.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        genderComboBox.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+        birthDatePicker.setOnMouseClicked (event -> {hideSignUpErrorLabels();});
+    }
 
-        genderComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue == null) {
-                //genderErrorLabel.setText("Gender must be selected");
-            } else {
-                //.setText("");
-            }
-        });
+    private void hideSignUpErrorLabels() {
+        nameErrorLabel.setText("");
+        emailErrorLabel.setText("");
+        passwordErrorLabel.setText("");
+        confirmPasswordErrorLabel.setText("");
+        phoneNumberErrorLabel.setText("");
+        genderAndbirthDateErrorLabel.setText("");
+    }
+
+    private void clearFieldData(){
+        userNameTextField.clear();
+        passwordField.clear();
+        passwordField.clear();
+        usernameField.clear();
+        phoneNumberTextField.clear();
+        emailTextField.clear();
+        signUpPasswordField.clear();
+        signUpPasswordTextVisible.clear();
+        signUpConfirmPasswordField.clear();
+        signUpConfirmPasswordTextVisible.clear();
+        genderComboBox.getSelectionModel();
+        birthDatePicker.getEditor().clear();
     }
 }
 
