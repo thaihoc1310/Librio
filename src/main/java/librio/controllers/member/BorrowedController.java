9bea0f59794ce16a0f345474d6904c3d9d9bafc8
huanh2106 <23020080@vnu.edu.vn>
2024-12-02 15:40:33 +0700

    update last

--- src/main/java/librio/controllers/member/BorrowedController.java
+++ src/main/java/librio/controllers/member/BorrowedController.java
@@ -37,7 +37,9 @@ import librio.session.Session;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -49,6 +51,7 @@ import java.util.concurrent.Executors;
 
 import static librio.enums.Status.*;
 import static librio.util.DatabaseUtil.checkIfUserRatedBook;
+import static librio.util.DatabaseUtil.updateQuantityBook;
 import static librio.util.DesignUtil.cropAndClipToCircle;
 import static librio.util.DesignUtil.cropToAspectRatio;
 
@@ -241,9 +244,9 @@ public class BorrowedController implements Initializable {
     }
 
     private void displayBorrowingBooks(List<BorrowedBook> booksToDisplay) {
+
         if (!booksToDisplay.isEmpty()) {
             bookBorrowVBox.getChildren().clear();
-
             for (BorrowedBook book : booksToDisplay) {
                 AnchorPane anchorPane = new AnchorPane();
                 anchorPane.setMinHeight(405);
@@ -636,13 +639,26 @@ public class BorrowedController implements Initializable {
 
 
             int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
+            if (rowsAffected >= 0) {
                 updateQuantityBook(borrowedBook.getId());
                 borrowBookList.remove(borrowedBook);
                 borrowedBook.setStatus(newStatus);
                 borrowedBook.setReturnDate(today);
                 returnedBookList.add(borrowedBook);
-                displayBorrowingBooks(borrowBookList);
+                if (borrowBookList.isEmpty()) {
+                    bookBorrowVBox.getChildren().clear();
+                    Label label = new Label("No Books Available");
+                    label.setPrefWidth(1260);
+                    label.setStyle("-fx-font-size:36;" +
+                            "-fx-font-weight: bold;");
+                    label.setPadding(new Insets(100, 0, 0, 0));
+                    label.setOpacity(0.41);
+                    label.setAlignment(Pos.CENTER);
+                    bookBorrowVBox.getChildren().add(label);
+
+                } else {
+                    displayBorrowingBooks(borrowBookList);
+                }
                 displayReturnedBooks(returnedBookList);
             } else {
                 System.out.println("Trả sách thất bại!");
